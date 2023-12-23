2ce2552d8a0980f4d0aa6a8b7b15848d911ac1ed
Martin Valov <martin.valov98@gmail.com>
2023-12-23 20:48:58 +0200

    Implementing logic for the buttons

--- index.js
+++ index.js
@@ -2,8 +2,21 @@
 const value = document.getElementById('value');
 const btns = document.querySelectorAll('input');
 
+
+const increase = (target) => {
+    target.value = Number(target.value) + 1;
+}
+
+const reset = (target) => {
+    target.value = 0;
+}
+
+const decrease = (target) => {
+    target.value = Number(target.value) - 1;
+}
+
 function onClick(ev) {
-    
+
 }
 
 btns.forEach(btn => {
