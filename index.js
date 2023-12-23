b1a1e8c39eb92cc9988fbea7834f150b2b9ce2c2
Martin Valov <martin.valov98@gmail.com>
2023-12-23 20:46:19 +0200

    attaching event listener to btns

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // selecting all the needed elements
 const value = document.getElementById('value');
 const btns = document.querySelectorAll('input');
+
+function onClick(ev) {
+    
+}
+
+btns.forEach(btn => {
+    btn.addEventListener('click', onClick);
+});
